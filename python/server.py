4339f28a3579d5220a492f0f67af2c09f0c42fda
Trevor Richardson <Tjrich336@gmail.com>
2024-05-22 13:13:31 -0400

    Fix: Port testing.

--- python/server.py
+++ python/server.py
@@ -867,4 +867,4 @@ def format_error(e):
 
 if __name__ == '__main__':
     # app.run(port=int(os.getenv('PORT', 8000)))
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', PORT=10000, debug=True)
